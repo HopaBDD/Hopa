c900e5b5ef97c69fb6de523c6f4a51942132c66a
ruslan.baryshnikov <ruslan.baryshnikov@eltex.loc>
2024-04-25 18:03:12 +0700

    Add expect

--- hopa/hopa.c
+++ hopa/hopa.c
@@ -4,13 +4,14 @@
 #include <stdint.h>
 #include <string.h>
 
-#define FUNK_NAME_HELPER_Y(_line)    hopa_func_##_line
+#define FUNK_NAME_HELPER_Y(_line)   hopa_func_##_line
 #define FUNK_NAME_HELPER_X(_line)   FUNK_NAME_HELPER_Y(_line)
 #define FUNK_NAME                   FUNK_NAME_HELPER_X(__LINE__)
 
 #define RUNIT_ATTACH_COUNT          10
 #define RUNIT_CHECK_FIRST_FAILURE   ru_count_failure != 1 ? true : printf("\nFailures:\n\n")
 #define RUNIT_ERROR_TEXT            printf("%d) %s\n\tError %s(%d): ", ru_count_failure, ru_err_buf, __FILE__, __LINE__)
+#define RUNIT_IF_EXPECT(_x,_y)      ru_is_expect == true ? ru_is_expect = false, printf("\n\tExpected: %s\n\tGot: %u\n\n\t(compared using %s)\n\n", _x, ru_expect_val, _y) : true
 
 
 #define to_eq(_x)                    == _x ? \
@@ -18,7 +19,8 @@
                                     (    ru_count_failure++, \
                                          RUNIT_CHECK_FIRST_FAILURE, \
                                          RUNIT_ERROR_TEXT, \
-                                         printf("Expected to equal to %s (%d dec/0x%x hex)\n", #_x, _x, _x) \
+                                         printf("Expected to equal to %s (%d dec/0x%x hex)\n", #_x, _x, _x), \
+                                         RUNIT_IF_EXPECT(#_x, "==") \
                                     ); \
                                     ru_test++;
 
@@ -27,7 +29,8 @@
                                     (   ru_count_failure++, \
                                         RUNIT_CHECK_FIRST_FAILURE, \
                                         RUNIT_ERROR_TEXT, \
-                                        printf("Expected to not to equal to %s (%d dec/0x%x hex)\n", #_x, _x, _x) \
+                                        printf("Expected to not to equal to %s (%d dec/0x%x hex)\n", #_x, _x, _x), \
+                                        RUNIT_IF_EXPECT(#_x, "!=") \
                                     ); \
                                     ru_test++;
 
@@ -68,6 +71,7 @@
 #define it(_str)                    ru_is_it = 1; \
                                     describe(_str) 
 
+#define expect(_s)                  ru_is_expect = true; ru_expect_val = _s; _s
 
 #define source(_str)
 
@@ -83,6 +87,8 @@ char           *ru_tmp_str[RUNIT_ATTACH_COUNT];
 char            ru_err_buf[1024] = "";
 unsigned int    ru_namespace = 0;
 char            ru_is_it = 0;
+unsigned int    ru_expect_val;
+bool            ru_is_expect = false;
 
 int main(void)
 {
